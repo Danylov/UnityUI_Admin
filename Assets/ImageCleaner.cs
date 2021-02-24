b0846397bea382496fe593458a12447ae2f9a04a
sodach1 <53436102+sodach1@users.noreply.github.com>
2021-02-24 12:20:55 +0200

    Other forms

---
+++ Assets/ImageCleaner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ImageCleaner : MonoBehaviour
+{
+    [SerializeField] Sprite defaultSprite;
+
+    [ContextMenu("CleanUpPanels")]
+    private void CleanUpPanels()
+    {
+        GameObject[] gos = FindObjectsOfType<GameObject>();
+
+        foreach (GameObject go in gos)
+        {
+            var bgSprite = go.GetComponent<Image>()?.sprite;
+
+            if (bgSprite != null)
+            {
+                if (bgSprite == defaultSprite)
+                {
+                    GetComponent<Image>().sprite = null;
+
+                    Debug.Log(go.name);
+                }
+            }
+        }
+        
+        Debug.Log(gos.Length);
+    }
+}
