31c235e2929bf9baabe8b8ac78c4187311b1605d
sodach1 <53436102+sodach1@users.noreply.github.com>
2021-02-25 09:40:15 +0200

    Upd forms

---
+++ Assets/Scripts/UI/Blocks/UserStudentBlock.cs
@@ -0,0 +1,65 @@
+﻿using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+namespace UI.Blocks
+{
+    public class UserStudentBlock : UnityEngine.MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI nameText;
+        [SerializeField] private TextMeshProUGUI loginText;
+        [SerializeField] private TextMeshProUGUI orgTypeText;
+        [SerializeField] private TextMeshProUGUI jobText;
+        [SerializeField] private TextMeshProUGUI tabNumText;
+
+        public TextMeshProUGUI NameText => nameText;
+        public TextMeshProUGUI LoginText => loginText;
+        public TextMeshProUGUI OrgTypeText => orgTypeText;
+        public TextMeshProUGUI JobText => jobText;
+        public TextMeshProUGUI TabNumText => tabNumText;
+
+        [SerializeField] private RectTransform blockContainer;
+        [SerializeField] private Transform changeButton;
+
+        private const float MinimizedHeight = 78f;
+        private const float MaximizedHeight = 267f;
+
+        private bool isMaximized = false;
+
+        private void Maximize()
+        {
+            isMaximized = true;
+
+            blockContainer.DOSizeDelta(new Vector2(blockContainer.sizeDelta.x,
+                MaximizedHeight), .15f);
+
+            loginText.transform.parent.gameObject.SetActive(true);
+            orgTypeText.transform.parent.gameObject.SetActive(true);
+            jobText.transform.parent.gameObject.SetActive(true);
+            tabNumText.transform.parent.gameObject.SetActive(true);
+            changeButton.gameObject.SetActive(true);
+        }
+
+        private void Minimize()
+        {
+            isMaximized = false;
+
+            blockContainer.DOSizeDelta(new Vector2(blockContainer.sizeDelta.x,
+                MinimizedHeight), .15f);
+
+            loginText.transform.parent.gameObject.SetActive(false);
+            orgTypeText.transform.parent.gameObject.SetActive(false);
+            jobText.transform.parent.gameObject.SetActive(false);
+            tabNumText.transform.parent.gameObject.SetActive(false);
+            changeButton.gameObject.SetActive(false);
+        }
+
+        public void SwitchState()
+        {
+            if (isMaximized)
+                Minimize();
+            else
+                Maximize();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/ToggleButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    [RequireComponent(typeof(Button))]
+    public class ToggleButton : MonoBehaviour
+    {
+        [SerializeField] private Button button;
+        [SerializeField] private Image tickImage;
+        [SerializeField] bool isOn;
+
+        public bool IsOn => isOn;
+
+        public void Start()
+        {
+            button.onClick.AddListener(Switch);
+        }
+
+        private void Switch()
+        {
+            isOn = !isOn;
+
+            tickImage.gameObject.SetActive(isOn);
+        }
+    }
+}
