b19d85216e214277ba7a7cde69f0ea58437662ef
Danylov <ol_danylov@ukr.net>
2021-02-25 14:02:53 +0200

    Change parent panel for prefab.

--- Assets/Scripts/UsersVis.cs
+++ Assets/Scripts/UsersVis.cs
@@ -9,11 +9,14 @@ using UnityEngine.UIElements;
 public class UsersVis : MonoBehaviour
 {
     private Register register;
+    private GameObject botPanelULR;
     public GameObject studentPrefab;
+    
     // Start is called before the first frame update
     void Start()
     {
         register = GameObject.Find("Registration").GetComponent<Register>();
+        botPanelULR = GameObject.Find("BotPanelULR");
         var query = string.Empty;
         try 
         { 
@@ -48,8 +51,15 @@ public class UsersVis : MonoBehaviour
     {
         Debug.Log(String.Format("{0}, {1}, {2}, {3}, {4}, {5}", 
             reader[0], reader[1], reader[2], reader[3], reader[4], reader[5])); // Отладка
-        var spawnLocation = new Vector3(30, 15*i, 0);
-        Instantiate(studentPrefab, spawnLocation, studentPrefab.transform.rotation);
+        var spawnLocation = new Vector3(0, 2*i, 0);
+        var studentInfo = Instantiate(studentPrefab, spawnLocation, Quaternion.identity);
+        studentInfo.transform.SetParent(botPanelULR.transform, false);
+        var stInfoBlock = studentInfo.GetComponent<UserStudentBlock>();
+        stInfoBlock.NameText.text = reader[0].ToString();
+        stInfoBlock.OrgTypeText.text = reader[1].ToString();
+        stInfoBlock.JobText.text = reader[2].ToString();
+        stInfoBlock.TabNumText.text = reader[3].ToString();
+        stInfoBlock.LoginText.text = reader[4].ToString();
     }
         
     public void ShowPanel()
