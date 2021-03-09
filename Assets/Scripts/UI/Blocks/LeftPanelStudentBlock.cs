325c213a58a4a7eb123cf88d3345f372f510423b
Romilk <53436102+sodach1@users.noreply.github.com>
2021-03-09 19:06:34 +0200

    Upd LeftStudentBlock API

--- Assets/Scripts/UI/Blocks/LeftPanelStudentBlock.cs
+++ Assets/Scripts/UI/Blocks/LeftPanelStudentBlock.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Animations;
+using UnityEngine.UI;
 
 public class LeftPanelStudentBlock : MonoBehaviour
 {
@@ -13,7 +14,17 @@ public class LeftPanelStudentBlock : MonoBehaviour
     [SerializeField] private TextMeshProUGUI pcNumText;
 
     [SerializeField] private Animator blockAnimator;
-    
+
+    [SerializeField] private Image rBlockImage;
+    [SerializeField] private Image blockImage;
+
+    [SerializeField] private Sprite activeBlockSprite;
+    [SerializeField] private Sprite inactiveBlockSprite;
+
+    private readonly Color activeColor = new Color32(255, 255, 255, 128);
+    private readonly Color inactiveColor = new Color32(255, 0, 61, 255);
+    private readonly int userEndedLab = Animator.StringToHash("UserEndedLab");
+
     //public StudentData data 
 
     /*public void FillData(StudentData data)
@@ -21,8 +32,25 @@ public class LeftPanelStudentBlock : MonoBehaviour
         this.data = data;
     }*/
 
-    public void StartStudentEndLabAnim()
+
+    private void SetOffline()
     {
-        // potom :]
+        rBlockImage.enabled = false;
+        blockImage.sprite = inactiveBlockSprite;
+
+        labTimer.color = inactiveColor;
+    }
+
+    private void SetOnline()
+    {
+        rBlockImage.enabled = true;
+        blockImage.sprite = activeBlockSprite;
+
+        labTimer.color = activeColor;
+    }
+
+    private void AnimateLabEnded()
+    {
+        blockAnimator.SetTrigger(userEndedLab);
     }
 }
