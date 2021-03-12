a9298a294855395a65d00ee5c8f4486d67ec5711
Danylov <ol_danylov@ukr.net>
2021-03-12 15:14:46 +0200

    Restore SQL script.

---
+++ Assets/SQLScripts/Students.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `students` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(45) DEFAULT NULL,
+  `organiztype` varchar(45) DEFAULT NULL,
+  `position` varchar(45) DEFAULT NULL,
+  `persnumber` int DEFAULT NULL,
+  `login` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `choosed` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci; 
